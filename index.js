d0f3045913205a5d64100e3edf2471a9d144676b
yashdobariya888@gmail.com <yashdoabriya888@gmail.com>
2021-08-04 19:40:45 +0530

    implimentaion of addtion oparatore

--- index.js
+++ index.js
@@ -1,3 +1,46 @@
-const demo = () => {
-    alert("hello good noon")
+let keys = []
+let keies;
+// let keies = keys.join()
+    
+const getSign = (event) => {
+    key = event.target.value;
+    console.log(key);
+    if (key === "=") {
+        console.log("barabar")
+        addition()
+    } else {
+        keys.push(parseInt(key))
+    }
+    // console.log(keys);
+    calculatorDisplay()
+}
+
+const calculatorDisplay = () => {
+    keies = keys.join("");
+    let display = document.getElementById("calculator_display");
+    display.value = keies
+}
+
+const addition = () => {
+    let data = keies.split("+")
+    console.log(data);
+    let add = data.reduce((a, b) => a + b);
+    console.log(add);
+    keys = []
+    keys.push(add)
+    console.log(add);
+    
+}
+const subtraction = () => {
+    
+}
+const multiplication = () => {
+    
+}
+const division = () => {
+    
+}
+
+const clear = () => {
+    
 }

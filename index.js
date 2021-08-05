c426a111ec2af84946fa394d48a7f94afeaa9267
yashdobariya888@gmail.com <yashdoabriya888@gmail.com>
2021-08-05 19:15:44 +0530

    all  opration done multipal opration bug con

--- index.js
+++ index.js
@@ -2,31 +2,52 @@ let key;
 let keys = []
 let keies;
 
+
 const getSign = (event) => {
     key = event.target.value;
-    console.log(key);
-    if (key == "=") {
-        
-        addition()
-
-    } else {
-        keys.push(key)
+    // console.log(key);
+    if (key == "" || key == "+" || key == "-" || key == "/" || key == "x") {
+        if (keies.includes("x")) {
+            multiplication()
+        }
+        else if (keies.includes("-")) {
+            subtraction()
+        }
+        else if (keies.includes("+")) {
+            addition()
+        }
+        else if (keies.includes("/")) {
+            division()
+        }
+    
     }
+    keys.push(key)
     calculatorDisplay()
 }
 
 const calculatorDisplay = () => {
+    if (key == "C") {
+        console.log(key);
+        keys = []
+    }
     keies = keys.join("");
     console.log(keies);
+
+    console.log(key);    
     let display = document.getElementById("calculator_display");
     display.value = keies
 }
+
+// console.log(keies.slice(-1), "sads");
+// let news = keies.replace(lastSign, key)
+// console.log(news)
 
 const addition = () => {
     let addItem = keies.split("+").map(x => +x);
     let add = addItem.reduce((a, b) => a + b);
     keys = [];
     keys.push(add);
+    console.log(keys);
     console.log("addtion");
 }
 
@@ -39,8 +60,11 @@ const subtraction = () => {
     
 }
 const multiplication = () => {
+    console.log(keies);
     let mulItem = keies.split("x").map(x => +x);
+    console.log(mulItem);
     let mul = mulItem.reduce((a, b) => a * b);
+    console.log(mul);
     keys = [];
     keys.push(mul);
     console.log("multiplication");
@@ -55,22 +79,9 @@ const division = () => {
     
 }
 
-const clear = () => {
-    
-}
-
-
-// keies.map(element => {
-//     if (element == "+") {
-//         addition()
-//     }
-//     else if (element == "-") {
-//         subtraction()
-//     }
-//     else if (element == "x") {
-//         multiplication()
-//     }
-//     else if (element == "/") {
-//         division()
-//     }
-// });
+    // let lastSign = keies.slice(-1)
+    // console.log(lastSign);
+    // if (lastSign == "x" && key == "-") {
+    //         keies.replace(lastSign, key)
+    //         console.log("minus");
+    //     }

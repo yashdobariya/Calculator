5700d60009d8b8b2a16e773442a6423dad82b476
yashdobariya888@gmail.com <yashdoabriya888@gmail.com>
2021-08-05 14:17:14 +0530

    update opration of add mul div sub

--- index.js
+++ index.js
@@ -1,59 +1,76 @@
+let key;
 let keys = []
 let keies;
-// let keies = keys.join()
-    
+
 const getSign = (event) => {
     key = event.target.value;
     console.log(key);
-    if (key == "x" || key == "-" || key == "+" || key == "/" || key ==  "C") {
-        // console.log(key);
-        // console.log(typeof(key));
-        keys.push(key)
-        // console.log("run")
-        // console.log(keys.push(typeof(parseInt(key))));
+    if (key == "=") {
+        
+        addition()
 
-    } else if (key == "=") {
-        // keys.push(key)
-    console.log("barabar")
-
-    addition()
-    }
-    else {
-        // console.log(key);
+    } else {
         keys.push(key)
-        // console.log(typeof(key));
-        // console.log("run")
     }
-    // console.log(keys);
     calculatorDisplay()
 }
 
 const calculatorDisplay = () => {
     keies = keys.join("");
+    console.log(keies);
     let display = document.getElementById("calculator_display");
     display.value = keies
 }
 
 const addition = () => {
-    let addItem = keies.split("+").map(x=>+x)
-    console.log(typeof(data[0]));
+    let addItem = keies.split("+").map(x => +x);
     let add = addItem.reduce((a, b) => a + b);
-    console.log(add);
-    keys = []
-    keys.push(add)
-    console.log(add);
-    
+    keys = [];
+    keys.push(add);
+    console.log("addtion");
 }
+
 const subtraction = () => {
+    let subItem = keies.split("-").map(x => +x);
+    let sub = subItem.reduce((a, b) => a - b);
+    keys = [];
+    keys.push(sub);
+    console.log("subtraction");
     
 }
 const multiplication = () => {
+    let mulItem = keies.split("x").map(x => +x);
+    let mul = mulItem.reduce((a, b) => a * b);
+    keys = [];
+    keys.push(mul);
+    console.log("multiplication");
     
 }
 const division = () => {
+    let divItem = keies.split("/").map(x => +x);
+    let divided = divItem.reduce((a, b) => a / b);
+    keys = [];
+    keys.push(divided);
+    console.log("division");
     
 }
 
 const clear = () => {
     
 }
+
+
+// keies.map(element => {
+//     if (element == "+") {
+//         addition()
+//     }
+//     else if (element == "-") {
+//         subtraction()
+//     }
+//     else if (element == "x") {
+//         multiplication()
+//     }
+//     else if (element == "/") {
+//         division()
+//     }
+// });
